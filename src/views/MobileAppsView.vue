16b1ca3b73ad6a5a8916e485f8707475ef577203
Ed Simmons <e.d.simmons@outlook.com>
2023-12-20 21:16:11 -0300

    Add: loader

--- src/views/MobileAppsView.vue
+++ src/views/MobileAppsView.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+import { reactive } from 'vue'
 import MobileAppInfo from '@/components/MobileAppInfo.vue'
 import MobileAppIcon from '@/components/MobileAppIcon.vue'
 import 'vue3-carousel/dist/carousel.css'
 import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
+import { PixelSpinner } from 'epic-spinners'
 
 interface MobileApp {
   name: string
@@ -19,7 +21,7 @@ const apps: MobileApp[] = [
     name: 'Opnametool IMG',
     icon: 'src/assets/MobileAppIcons/opnametool.webp',
     description:
-      "This iOS app was a bespoke project for the Instituut Mijnbouwschade Groningen (IMG), a department of the Dutch government. It is used by their team of experts to assess the damage caused to people's homes by earthquakes, and to calculate any compensation that they are owed.",
+      "This iOS app was a bespoke project for the Instituut Mijnbouwschade Groningen (IMG), a department of the Dutch government. It is used by their team of experts to assess the damage caused to people's homes by earthquakes, and to calculate any compensation that they are owed. Over €1b has been awarded as a result.",
     url: {
       ios: 'https://apps.apple.com/us/app/opnametool-img/id1605604226?itsct=apps_box_badge&amp;itscg=30200'
     }
@@ -62,56 +64,79 @@ const apps: MobileApp[] = [
       'Developed solely for iOS, this app was designed for the Van der Spek funeral company, and is used by their employees to manage their appointments, track hours worked, and view details of the deceased. This was my first project at Atabix.'
   }
 ]
+
+const state = reactive({
+  isLoading: true
+})
+
+const onCarouselInit = () => {
+  setTimeout(() => {
+    state.isLoading = false
+  }, 1000)
+}
 </script>
 
 <template>
-  <carousel :items-to-show="1" :wrap-around="true">
-    <slide v-for="app in apps" :key="app.name">
-      <mobile-app-info>
-        <template #icon>
-          <mobile-app-icon :img="app.icon" />
-        </template>
-        <template #heading>{{ app.name }}</template>
-        {{ app.description }}
-        <template #links v-if="app.url">
-          <div class="app-store-links-container">
-            <div v-if="app.url.android" class="app-store-android">
-              <a :href="app.url.android" class="app-store-link"
-                ><img
-                  alt="Get it on Google Play"
-                  src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png"
-              /></a>
+  <div class="relative">
+    <pixel-spinner
+      :animation-duration="2000"
+      :size="70"
+      color="#5c8d89"
+      :class="state.isLoading ? 'loader' : 'loader finished'"
+    />
+    <carousel
+      @init="onCarouselInit"
+      :items-to-show="1"
+      :wrap-around="true"
+      :class="state.isLoading ? 'carousel-loading' : 'carousel-finished'"
+    >
+      <slide v-for="app in apps" :key="app.name">
+        <mobile-app-info>
+          <template #icon>
+            <mobile-app-icon :img="app.icon" />
+          </template>
+          <template #heading>{{ app.name }}</template>
+          {{ app.description }}
+          <template #links v-if="app.url">
+            <div class="app-store-links-container">
+              <div v-if="app.url.android" class="app-store-android">
+                <a :href="app.url.android" class="app-store-link"
+                  ><img
+                    alt="Get it on Google Play"
+                    src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png"
+                /></a>
+              </div>
+              <div v-if="app.url.ios" class="app-store-ios">
+                <a
+                  :href="app.url.ios"
+                  style="
+                    display: inline-block;
+                    overflow: hidden;
+                    border-radius: 13px;
+                    width: 250px;
+                    height: 83px;
+                  "
+                  class="app-store-link"
+                  ><img
+                    src="https://tools.applemediaservices.com/api/badges/download-on-the-app-store/black/en-us?size=250x83&amp;releaseDate=1559347200"
+                    alt="Download on the App Store"
+                    style="border-radius: 13px; width: 250px; height: 83px"
+                /></a>
+              </div>
             </div>
-            <div v-if="app.url.ios" class="app-store-ios">
-              <a
-                :href="app.url.ios"
-                style="
-                  display: inline-block;
-                  overflow: hidden;
-                  border-radius: 13px;
-                  width: 250px;
-                  height: 83px;
-                "
-                class="app-store-link"
-                ><img
-                  src="https://tools.applemediaservices.com/api/badges/download-on-the-app-store/black/en-us?size=250x83&amp;releaseDate=1559347200"
-                  alt="Download on the App Store"
-                  style="border-radius: 13px; width: 250px; height: 83px"
-              /></a>
-            </div>
-          </div>
-        </template>
-      </mobile-app-info>
-    </slide>
+          </template>
+        </mobile-app-info>
+      </slide>
 
-    <template #addons>
-      <navigation />
-      <pagination />
-    </template>
-  </carousel>
+      <template #addons>
+        <navigation />
+        <pagination />
+      </template>
+    </carousel>
+  </div>
 </template>
 
-<style scoped>
+<style>
 .app-store-links-container {
   display: flex;
   flex-direction: row;
@@ -128,5 +153,32 @@ const apps: MobileApp[] = [
 
 .app-store-link {
   background-color: #ffffff;
+}
+
+.carousel__prev,
+.carousel__next {
+  margin: 0;
+}
+
+.loader {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  opacity: 1;
+}
+
+.finished {
+  opacity: 0;
+  transition: all 1s;
+}
+
+.carousel-loading {
+  opacity: 0;
+}
+
+.carousel-finished {
+  opacity: 1;
+  transition: all 1s;
 }
 </style>
