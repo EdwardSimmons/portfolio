a02f3bc13b354a935b320e672875d8490cbcb591
Ed Simmons <e.d.simmons@outlook.com>
2023-12-22 11:16:22 -0300

    Fix: FontAwesome imports

---
+++ src/FontAwesome.ts
@@ -0,0 +1,40 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {
+  faAddressBook,
+  faUser,
+  faBriefcase,
+  faGraduationCap,
+  faPhone,
+  faEnvelope,
+  faScrewdriverWrench,
+  faAward,
+  faMobile,
+  faCarSide,
+  faChevronRight,
+  faChevronLeft,
+  faBars
+} from '@fortawesome/free-solid-svg-icons'
+import { faSmile } from '@fortawesome/free-regular-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
+library.add(
+  faSmile,
+  faGithub,
+  faLinkedin,
+  faAddressBook,
+  faUser,
+  faBriefcase,
+  faGraduationCap,
+  faPhone,
+  faEnvelope,
+  faScrewdriverWrench,
+  faAward,
+  faMobile,
+  faCarSide,
+  faChevronRight,
+  faChevronLeft,
+  faBars
+)
+
+export default FontAwesomeIcon
--- src/main.ts
+++ src/main.ts
@@ -3,43 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import {
-  faAddressBook,
-  faUser,
-  faBriefcase,
-  faGraduationCap,
-  faPhone,
-  faEnvelope,
-  faScrewdriverWrench,
-  faAward,
-  faMobile,
-  faCarSide,
-  faChevronRight,
-  faChevronLeft
-} from '@fortawesome/free-solid-svg-icons'
-import { faSmile } from '@fortawesome/free-regular-svg-icons'
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
-
-library.add(
-  faSmile,
-  faGithub,
-  faLinkedin,
-  faAddressBook,
-  faUser,
-  faBriefcase,
-  faGraduationCap,
-  faPhone,
-  faEnvelope,
-  faScrewdriverWrench,
-  faAward,
-  faMobile,
-  faCarSide,
-  faChevronRight,
-  faChevronLeft
-)
+import FontAwesomeIcon from './FontAwesome'
 
 const app = createApp(App)
 
